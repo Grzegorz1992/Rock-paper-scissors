8a95cbd973199fcaeae779522e21758694da1ab1
Grzegorz <gkopacki92@gmail.com>
2023-12-18 00:08:17 +0100

    added modal items in index.html and styled them in style.css. In main.js added two functions for show and close rules-modal.I also change background for body(i choose darkest colours)

--- src/main.js
+++ src/main.js
@@ -1,1 +1,19 @@
-import './style.css'
+import "./style.css";
+
+const rulesBtn = document.querySelector(".rules-btn");
+const rulesModal = document.querySelector(".modal-rules-box");
+const closeModalBtn = document.querySelector(".close-modal-btn");
+const body = document.querySelector('body')
+
+const showModal = () => {
+	rulesModal.classList.add("active");
+    body.classList.add('modal-open')
+};
+
+const closeModal = () => {
+    rulesModal.classList.remove('active')
+    body.classList.remove('modal-open')
+}
+
+rulesBtn.addEventListener("click", showModal);
+closeModalBtn.addEventListener("click", closeModal);
