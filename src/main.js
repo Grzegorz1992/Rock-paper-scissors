52955b721956590184daa31f0a52caf3eb39c002
Grzegorz <gkopacki92@gmail.com>
2023-12-19 14:12:32 +0100

    created userChoice function and add style element in gameItems objects

--- src/main.js
+++ src/main.js
@@ -1,9 +1,13 @@
 import "./style.css";
 
 const gameItems = [
-	{ id: "paper", image: "fa-solid fa-hand" },
-	{ id: "scissors", image: "fa-solid fa-hand-scissors" },
-	{ id: "rock", image: "fa-solid fa-hand-back-fist" },
+	{ id: "paper", image: "fa-solid fa-hand", style: "paper-item" },
+	{
+		id: "scissors",
+		image: "fa-solid fa-hand-scissors",
+		style: "scissors-item",
+	},
+	{ id: "rock", image: "fa-solid fa-hand-back-fist", style: "rock-item" },
 ];
 
 const rulesBtn = document.querySelector(".rules-btn");
@@ -13,6 +17,7 @@ const body = document.querySelector("body");
 const rpsItems = document.querySelectorAll(".game-item");
 const fightGameBox = document.querySelector(".fight-game-box");
 const gameItemsBox = document.querySelector(".game-items-box");
+const userItemSelect = document.querySelector(".user-choice");
 
 const showModal = () => {
 	rulesModal.classList.add("active");
@@ -28,6 +33,15 @@ const userChoice = (e) => {
 	let selectedItem = e.target.dataset.id;
 	gameItemsBox.classList.add("hide-gime-items");
 	fightGameBox.classList.add("show-fight-game-box");
+	for (let i = 0; i < gameItems.length; i++) {
+		if (gameItems[i].id === selectedItem) {
+			userItemSelect.innerHTML = `		<div class="fight-game-item ${gameItems[i].style}" id="${gameItems[i].id}">
+            <div class="fight-image-container">
+                <p class="fight-game-image"><i class="${gameItems[i].image}"></i></p>
+            </div>
+        </div>`;
+		}
+	}
 
 	console.log(selectedItem);
 };
