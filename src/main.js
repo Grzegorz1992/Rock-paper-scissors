fdfcd7b84bd03db2a760d808ed11f8f992c22cc8
Grzegorz <gkopacki92@gmail.com>
2023-12-19 12:50:42 +0100

    in main.js file tables with objects were created to dynamically redraw game elements, in index.html created play-again div and button and styled them in style.css

--- src/main.js
+++ src/main.js
@@ -1,19 +1,25 @@
 import "./style.css";
 
+const gameItems = [
+	{ id: "paper", image: "fa-solid fa-hand" },
+	{ id: "scissors", image: "fa-solid fa-hand-scissors" },
+	{ id: "rock", image: "fa-solid fa-hand-back-fist" },
+];
+
 const rulesBtn = document.querySelector(".rules-btn");
 const rulesModal = document.querySelector(".modal-rules-box");
 const closeModalBtn = document.querySelector(".close-modal-btn");
-const body = document.querySelector('body')
+const body = document.querySelector("body");
 
 const showModal = () => {
 	rulesModal.classList.add("active");
-    body.classList.add('modal-open')
+	body.classList.add("modal-open");
 };
 
 const closeModal = () => {
-    rulesModal.classList.remove('active')
-    body.classList.remove('modal-open')
-}
+	rulesModal.classList.remove("active");
+	body.classList.remove("modal-open");
+};
 
 rulesBtn.addEventListener("click", showModal);
 closeModalBtn.addEventListener("click", closeModal);
