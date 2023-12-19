a0e77b843e02304c226376a44329c7929d5060c8
Grzegorz <gkopacki92@gmail.com>
2023-12-19 14:39:21 +0100

    created houseChoice function

--- src/main.js
+++ src/main.js
@@ -1,13 +1,19 @@
 import "./style.css";
 
 const gameItems = [
-	{ id: "paper", image: "fa-solid fa-hand", style: "paper-item" },
+	{ id: "paper", image: "fa-solid fa-hand", style: "paper-item", index: 0 },
 	{
 		id: "scissors",
 		image: "fa-solid fa-hand-scissors",
 		style: "scissors-item",
+		index: 1,
 	},
-	{ id: "rock", image: "fa-solid fa-hand-back-fist", style: "rock-item" },
+	{
+		id: "rock",
+		image: "fa-solid fa-hand-back-fist",
+		style: "rock-item",
+		index: 2,
+	},
 ];
 
 const rulesBtn = document.querySelector(".rules-btn");
@@ -18,6 +24,7 @@ const rpsItems = document.querySelectorAll(".game-item");
 const fightGameBox = document.querySelector(".fight-game-box");
 const gameItemsBox = document.querySelector(".game-items-box");
 const userItemSelect = document.querySelector(".user-choice");
+const houseItemSelect = document.querySelector(".house-choice");
 
 const showModal = () => {
 	rulesModal.classList.add("active");
@@ -43,7 +50,22 @@ const userChoice = (e) => {
 		}
 	}
 
-	console.log(selectedItem);
+	setTimeout(() => {
+		houseChoice();
+	}, 1000);
+};
+
+const houseChoice = () => {
+	let randomChoice = Math.floor(Math.random() * gameItems.length);
+	for (let i = 0; i < gameItems.length; i++) {
+		if (randomChoice === gameItems[i].index) {
+			houseItemSelect.innerHTML = `<div class="fight-game-item ${gameItems[i].style}" id="${gameItems[i].id}">
+            <div class="fight-image-container">
+                <p class="fight-game-image"><i class="${gameItems[i].image}"></i></p>
+            </div>
+        </div>`;
+		}
+	}
 };
 
 rulesBtn.addEventListener("click", showModal);
