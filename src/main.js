45fdc6de2b6fb25b54761f0c4d3884f2771be42f
Grzegorz <gkopacki92@gmail.com>
2023-12-19 13:52:56 +0100

    added listening for game items, added transition from gameitemsbox to fightgamebox

--- src/main.js
+++ src/main.js
@@ -10,6 +10,9 @@ const rulesBtn = document.querySelector(".rules-btn");
 const rulesModal = document.querySelector(".modal-rules-box");
 const closeModalBtn = document.querySelector(".close-modal-btn");
 const body = document.querySelector("body");
+const rpsItems = document.querySelectorAll(".game-item");
+const fightGameBox = document.querySelector(".fight-game-box");
+const gameItemsBox = document.querySelector(".game-items-box");
 
 const showModal = () => {
 	rulesModal.classList.add("active");
@@ -21,5 +24,14 @@ const closeModal = () => {
 	body.classList.remove("modal-open");
 };
 
+const userChoice = (e) => {
+	let selectedItem = e.target.dataset.id;
+	gameItemsBox.classList.add("hide-gime-items");
+	fightGameBox.classList.add("show-fight-game-box");
+
+	console.log(selectedItem);
+};
+
 rulesBtn.addEventListener("click", showModal);
 closeModalBtn.addEventListener("click", closeModal);
+rpsItems.forEach((item) => item.addEventListener("click", userChoice));
